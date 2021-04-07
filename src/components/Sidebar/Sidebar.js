4d7c75a7c22f43725f127648eec8bc8d9b32c889
Vee <avhadpriyanka88@gmail.com>
2021-04-07 17:25:03 +0530

    first commit

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,169 @@
+import React from 'react';
+import cx from 'classnames';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import {Progress, Alert} from 'reactstrap';
+import {withRouter} from 'react-router-dom';
+import {dismissAlert} from '../../actions/alerts';
+import s from './Sidebar.module.scss';
+import LinksGroup from './LinksGroup';
+import Button from 'reactstrap/lib/Button';
+import PowerIcon from "../Icons/HeaderIcons/PowerIcon";
+
+import {changeActiveSidebarItem} from '../../actions/navigation';
+import {logoutUser} from '../../actions/user';
+import HomeIcon from '../Icons/SidebarIcons/HomeIcon';
+import TypographyIcon from '../Icons/SidebarIcons/TypographyIcon';
+import TablesIcon from '../Icons/SidebarIcons/TablesIcon';
+import NotificationsIcon from '../Icons/SidebarIcons/NotificationsIcon';
+import ComponentsIcon from '../Icons/SidebarIcons/ComponentsIcon';
+
+
+
+class Sidebar extends React.Component {
+    static propTypes = {
+        sidebarStatic: PropTypes.bool,
+        sidebarOpened: PropTypes.bool,
+        dispatch: PropTypes.func.isRequired,
+        activeItem: PropTypes.string,
+        location: PropTypes.shape({
+            pathname: PropTypes.string,
+        }).isRequired,
+    };
+
+    static defaultProps = {
+        sidebarStatic: false,
+        activeItem: '',
+    };
+
+    constructor(props) {
+        super(props);
+
+        this.doLogout = this.doLogout.bind(this);
+    }
+
+    componentDidMount() {
+        this.element.addEventListener('transitionend', () => {
+            if (this.props.sidebarOpened) {
+                this.element.classList.add(s.sidebarOpen);
+            }
+        }, false);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.sidebarOpened !== this.props.sidebarOpened) {
+            if (nextProps.sidebarOpened) {
+                this.element.style.height = `${this.element.scrollHeight}px`;
+            } else {
+                this.element.classList.remove(s.sidebarOpen);
+                setTimeout(() => {
+                    this.element.style.height = '';
+                }, 0);
+            }
+        }
+    }
+
+    dismissAlert(id) {
+        this.props.dispatch(dismissAlert(id));
+    }
+
+    doLogout() {
+        this.props.dispatch(logoutUser());
+    }
+
+    render() {
+        return (
+            <nav
+                className={cx(s.root)}
+                ref={(nav) => {
+                    this.element = nav;
+                }}
+            >
+                <header className={s.logo}>
+                    <h4>Carbon Footprint</h4>
+                </header>
+                <ul className={s.nav}>
+                    <LinksGroup
+                        onActiveSidebarItemChange={activeItem => this.props.dispatch(changeActiveSidebarItem(activeItem))}
+                        activeItem={this.props.activeItem}
+                        header="HomePage"
+                        isHeader
+                        iconName={<HomeIcon className={s.menuIcon} />}
+                        link="/app/main"
+                        index="main"
+                    />
+                    <LinksGroup
+                        onActiveSidebarItemChange={activeItem => this.props.dispatch(changeActiveSidebarItem(activeItem))}
+                        activeItem={this.props.activeItem}
+                        header="Typography"
+                        isHeader
+                        iconName={<TypographyIcon className={s.menuIcon} />}
+                        link="/app/typography"
+                        index="core"
+                    />
+                    <LinksGroup
+                        onActiveSidebarItemChange={t => this.props.dispatch(changeActiveSidebarItem(t))}
+                        activeItem={this.props.activeItem}
+                        header="Tables Basic"
+                        isHeader
+                        iconName={<TablesIcon className={s.menuIcon} />}
+                        link="/app/tables"
+                        index="tables"
+                    />
+                    <LinksGroup
+                        onActiveSidebarItemChange={activeItem => this.props.dispatch(changeActiveSidebarItem(activeItem))}
+                        activeItem={this.props.activeItem}
+                        header="Notifications"
+                        isHeader
+                        iconName={<NotificationsIcon className={s.menuIcon}/>}
+                        link="/app/notifications"
+                        index="ui"
+                    />
+                    <LinksGroup
+                        onActiveSidebarItemChange={activeItem => this.props.dispatch(changeActiveSidebarItem(activeItem))}
+                        activeItem={this.props.activeItem}
+                        header="Components"
+                        isHeader
+                        iconName={<ComponentsIcon className={s.menuIcon}/>}
+                        link="/app/components"
+                        index="components"
+                        childrenLinks={[
+                            {
+                                header: 'Charts', link: '/app/components/charts',
+                            },
+                            {
+                                header: 'Icons', link: '/app/components/icons',
+                            },
+                            {
+                                header: 'Maps', link: '/app/components/maps',
+                            },
+                        ]}
+                    />
+                    <Button  className={s.logbtn} onClick = {this.doLogout}>
+                     <LinksGroup
+                        onActiveSidebarItemChange={activeItem => this.props.dispatch(changeActiveSidebarItem(activeItem))}
+                        activeItem={this.props.activeItem}
+                        header="Logout"
+                        isHeader
+                        iconName={<PowerIcon className={s.menuIcon}/>}
+                        link="/login"
+                       
+                        index="logout"
+                    />
+                    </Button>
+                </ul>
+            </nav>
+        );
+    }
+}
+
+function mapStateToProps(store) {
+    return {
+        sidebarOpened: store.navigation.sidebarOpened,
+        sidebarStatic: store.navigation.sidebarStatic,
+        alertsList: store.alerts.alertsList,
+        activeItem: store.navigation.activeItem,
+    };
+}
+
+export default withRouter(connect(mapStateToProps)(Sidebar));
---
+++ src/pages/dashboard/components/am4chartMap/mock.js
@@ -0,0 +1,63 @@
+export default [
+  {
+    latitude: 19.0760,
+    longitude: 72.8777,
+    size: 6,
+    tooltip: 'Mumbai',
+    fill: "#F00"
+  },
+  {
+    latitude: 28.7041,
+    longitude: 77.1025,
+    size: 7,
+    tooltip: 'Delhi',
+  },
+  {
+    latitude: 18.5204,
+    longitude: 73.8567,
+    size: 4,
+    tooltip: 'Pune',
+  },
+  {
+    latitude: 22.5726,
+    longitude: 88.3639,
+    size: 9,
+    tooltip: 'Kolkata',
+  },
+  {
+    latitude: 17.3850,
+    longitude: 78.4867,
+    size: 4,
+    tooltip: 'Hyderabad',
+  },
+  {
+    latitude: 12.9716,
+    longitude: 77.5946,
+    size: 8,
+    tooltip: 'Bengaluru',
+  },
+  {
+    latitude: 23.0225,
+    longitude: 72.5714,
+    size: 6,
+    tooltip: 'Ahmedabad',
+  },
+  {
+    latitude: 13.0827,
+    longitude: 80.2707,
+    size: 5,
+    tooltip: 'Chennai',
+  },
+  {
+    latitude: 26.8467,
+    longitude: 80.9462,
+    size: 5,
+    tooltip: 'Lucknow',
+  },
+  {
+    latitude: 17.6868,
+    longitude: 83.2185,
+    size: 6,
+    tooltip: 'Visakhapatnam',
+  }
+]
